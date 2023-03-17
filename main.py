3ea6ac520efbf7c3128dadf291a230cd8665d7b0
Ryuuz02 <ryuuz02@outlook.com>
2023-03-17 14:25:13 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+# array makes an array using the list given
+# test_array = np.array([1, 2, 3])
+# test_array = np.array([[1, 2, 3], [4, 5, 6]])
+
+# zeros makes an array of zeros with the given shape
+# test_array = np.zeros((2, 2))
+
+# ones makes an array of ones with the given shape
+# test_array = np.ones((2, 2))
+
+# arange makes an array using the given range
+# test_array = np.arange(0, 10, 2)
+
+# ndarray makes an array using the shape given
+# test_array = np.ndarray((2, 5, 5), dtype=int)
+
+# fill fills the array with the given value
+# test_array.fill(1)
+
+# calling an index returns the axis matching that index
+# print(test_array[0])
+
+# calling two indexes returns the value at that index
+# print(test_array[1][1])
+
+# calling a slice returns the values in that slice
+# print(test_array[0:2])
+
+# calling a slice with a step returns the values in that slice with the given step
+# print(test_array[0:4:2])
+
+# can replace axes in an array using other axes of the array 
+# ones_array = np.ones((5, 5))
+# zero_array = np.zeros((5, 5))
+# test_array[0] = ones_array
+# test_array[1] = zero_array
+
+# print(test_array)
